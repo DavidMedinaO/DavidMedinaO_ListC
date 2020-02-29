efee53f5ae9fcb463c3300c1d96fc1595404af21
DavidMedinaO <53974843+DavidMedinaO@users.noreply.github.com>
2020-02-29 10:33:07 -0500

    Estructuras

--- Listas/src/listas/Listas.java
+++ Listas/src/listas/Listas.java
@@ -173,7 +173,7 @@ public class Listas {
     
     public static final String ANSI_BLUE = "\u001B[34m";
     public static final String ANSI_RESET = "\u001B[0m";
-     public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
 
 
 }
